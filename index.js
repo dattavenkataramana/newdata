43078f1873dbfda88b6d8d9d9c9e545ac8ebda16
dattavenkataramana <bavirisettidvramana@gmail.com>
2024-03-14 11:09:13 +0530

    new data

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const {open} = require('sqlite');
+const sqlite3 = require('sqlite3')
+
+const app = express();
+
+app.use(express.json()); 
+
+const dbPath = path.join(__dirname,"database.db");
+
+let db = null 
+
+const initializeDbAndServer = async ()=>{
+    try{
+        db = await open({
+            filename:dbPath,
+            driver:sqlite3.Database,
+        }) 
+        app.listen(4000,()=>{
+            console.log(`server running at http://localhost:4000`)
+        })
+    }catch(e){
+        console.log(`DB Error :${e.message}`);
+        process.exit(1)
+    }
+}
+
+
+initializeDbAndServer()
+
+app.get("/login1/", async (req, res) => {
+  
+    const getQueryDatta = "SELECT * FROM  login1";
+    const datas = await db.all(getQueryDatta);
+
+    res.send(datas);
+});
+
+
+app.post('/login1/', async (req, res) => {
+    try {
+      const { name, password } = req.body;
+      const addData = `INSERT INTO login1(name, password) VALUES(?, ?)`;
+      await db.run(addData, [name, password]);
+      res.status(201).send('User added successfully!');
+    } catch (error) {
+      console.error('Error adding user:', error);
+      res.status(500).send('Failed to add user!');
+    }
+  });
